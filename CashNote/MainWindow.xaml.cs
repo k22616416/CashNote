0e732a1010f47f49fa8a7fae296d24b2854d5ed1
陳 廷偉 <kk013579@gmail.com>
2020-05-13 02:20:54 +0800

    加入專案檔案。

---
+++ CashNote/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashNote
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const String CustomItemName = "CustomItem";
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button[] btn = new Button[] { ItemBtn1, ItemBtn2, ItemBtn3, ItemBtn4 };
+            //this.AddHandler(Button.ClickEvent, new RoutedEventHandler(this.ItemBtn_Click));
+            
+        }
+
+        private void ItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(btn.Name == CustomItemName)
+            {
+                TestLabel.Content = System.DateTime.Now + " " + InputCash.Text + " " + InputItem.Text;
+                InputCash.Text = "";
+            }
+            else
+            {
+                TestLabel.Content = System.DateTime.Now + " " + InputCash.Text + " " + btn.Content;
+                InputCash.Text = "";
+            }
+            
+            /*
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            TestLabel.Content = feSource.ContextMenu;
+            */
+
+
+
+        }
+    }
+}
